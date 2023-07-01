a91e3e3ebacc58aa6068496074a094777133d321
Graimi <113901525+Graimi@users.noreply.github.com>
2023-07-01 21:25:02 +0200

    prettier and eslint applied

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off'
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gm-container">
+      <h1>Tres en raya</h1>
+      <h1>Sudoku</h1>
+      <h1>Ahorcado</h1>
+    </div>
+  );
+}
+
+export default App;
