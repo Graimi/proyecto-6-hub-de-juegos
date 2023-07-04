568885e9c46bdff2641e4802a389c39035d6e25c
Graimi <113901525+Graimi@users.noreply.github.com>
2023-07-04 21:09:01 +0200

    game created

--- src/pages/TicTacToe/TicTacToe.jsx
+++ src/pages/TicTacToe/TicTacToe.jsx
@@ -7,6 +7,20 @@ function TicTacToe() {
   const [player, setPlayer] = useState('');
   const [data, setData] = useState([null, null, null, null, null, null, null, null, null]);
 
+  const winningCombinations = [
+    // Combinaciones horizontales
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Combinaciones verticales
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Combinaciones diagonales
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
   function getRandomPlayer() {
     const players = ['❎', '🔴'];
     const randomIndex = Math.floor(Math.random() * players.length);
@@ -15,50 +29,50 @@ function TicTacToe() {
 
   function resetData() {
     setData([null, null, null, null, null, null, null, null, null]);
-    const board = document.querySelector('.gm-tictactoe-board');
-    board.style.display = 'grid';
+    const board = document.querySelector('.none');
+    board.style.display = 'flex';
     setPlayer(getRandomPlayer());
   }
 
   function checkWinner() {
-    // const winningCombinations = [
-    console.log(data[0], data[1], data[2]);
-    // if ((data[0] === '🔴') & (data[1] === '🔴') & (data[2] === '🔴')) {
-    if ((data[0] == data[1]) == data[2]) {
-      console.log('ganador', { player });
-      resetData();
-    }
+    winningCombinations.forEach((combination) => {
+      const [index1, index2, index3] = combination;
+      if (data[index1] === player && data[index2] === player && data[index3] === player) {
+        console.log(`Ganador: ${player}`);
+        resetData();
+      }
+    });
   }
 
   return (
     <div className="gm-tictactoe">
       <h1>Tres en raya</h1>
-      {/* <button type="button">Empezar partida</button> */}
-      {/* <button type="reset"><p>Nueva partida</p></button> */}
-      <div id="board" className="gm-tictactoe-board">
-        {data.map((space, index) => {
-          return (
-            // eslint-disable-next-line jsx-a11y/control-has-associated-label
-            <button
-              type="button"
-              id="cell"
-              className={`cell-${index}`}
-              onClick={() => {
-                // const updatedData = [...data];
-                // updatedData[index] = player;
-                // setData(updatedData);
-                data[index] = `${player}`;
-                setPlayer(player === '❎' ? '🔴' : '❎');
-                console.log(data);
-                checkWinner();
-              }}
-            >
-              {space}
-            </button>
-          );
-        })}
+      <div className="none">
+        <div id="board" className="gm-tictactoe-board">
+          {data.map((space, index) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/control-has-associated-label
+              <button
+                type="button"
+                id="cell"
+                className={`cell-${index}`}
+                onClick={() => {
+                  // const updatedData = [...data];
+                  // updatedData[index] = player;
+                  // setData(updatedData);
+                  data[index] = `${player}`;
+                  setPlayer(player === '❎' ? '🔴' : '❎');
+                  console.log(data);
+                  checkWinner();
+                }}
+              >
+                {space}
+              </button>
+            );
+          })}
+        </div>
+        <h3>Turno: {player}</h3>
       </div>
-      <h3>Turno: {player}</h3>
       <button type="button" onClick={() => resetData()}>
         <p>Nueva partida</p>
       </button>
