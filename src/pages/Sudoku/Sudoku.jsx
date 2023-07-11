f234d77e20382b2a65966f2d282005598f33d02e
Graimi <113901525+Graimi@users.noreply.github.com>
2023-07-11 18:27:21 +0200

    sudoku game done

--- src/pages/Sudoku/Sudoku.jsx
+++ src/pages/Sudoku/Sudoku.jsx
@@ -1,132 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import './Sudoku.css';
-// import { Link } from 'react-router-dom';
-// import { makepuzzle, solvepuzzle } from 'sudoku';
-
-// function Sudoku() {
-//   const [sudokuBoard, setSudokuBoard] = useState(null);
-//   const [newSudokuBoard, setNewSudokuBoard] = useState(null);
-//   const [winnerMessage, setWinnerMessage] = useState('');
-//   const [result, setResult] = useState('');
-
-//   useEffect(() => {
-//     setSudokuBoard(makepuzzle());
-//   }, []);
-
-//   const handleInputChange = (event, index) => {
-//     // const newBoard = [...sudokuBoard];
-//     newSudokuBoard[index] = Number(event.target.value);
-//     // setNewSudokuBoard(newBoard);
-//   };
-
-//   const renderCell = (number, index) => {
-//     // const originalNumber = number === sudokuBoard[index] && number !== null;
-
-//     if (number === sudokuBoard[index] && number !== null) {
-//       return (
-//         <div className="sudoku-cell-original" key={index}>
-//           {number}
-//         </div>
-//       );
-//     }
-//     if (number) {
-//       return (
-//         <div className="sudoku-cell" key={index}>
-//           {number}
-//         </div>
-//       );
-//     }
-//     return (
-//       <input
-//         key={index}
-//         type="number"
-//         className="sudoku-cell"
-//         min="1"
-//         max="9"
-//         // disabled={originalNumber}
-//         onChange={(event) => handleInputChange(event, index)}
-//         // onChange={(event) => number = event.target.value}
-//       />
-//     );
-//   };
-
-//   function checkSudoku() {
-//     // Comprobamos si solved es truthy, y sudokuBoard está correctamente solucionado
-//     const solved = solvepuzzle(sudokuBoard);
-//     console.log('Solved', solved);
-//     console.log('Original', sudokuBoard);
-//     console.log('New', newSudokuBoard);
-//     if (JSON.stringify(solved) === JSON.stringify(newSudokuBoard)) {
-//       setWinnerMessage('Resuelto, enhorabuena');
-//       setResult(true);
-//     } else {
-//       setWinnerMessage('Erróneo');
-//       setResult(false);
-//     }
-//   }
-
-//   function newGame() {
-//     setSudokuBoard(null);
-//     setSudokuBoard(makepuzzle());
-//     // setNewSudokuBoard(null);
-//     // setNewSudokuBoard([...sudokuBoard]);
-//   }
-
-//   useEffect(() => {
-//     setNewSudokuBoard(sudokuBoard);
-//   }, [newGame]);
-
-//   // Función encargada de volver a poner el mensaje del ganador vacío
-//   function closeWinnerMessage() {
-//     setWinnerMessage('');
-//   }
-
-//   function resolveSudoku() {
-//     setNewSudokuBoard(solvepuzzle(sudokuBoard));
-//     closeWinnerMessage();
-//   }
-
-//   return (
-//     sudokuBoard &&
-//     newSudokuBoard && (
-//       <div className="gm-sudoku">
-//         <article className="gm-sudoku-game">
-//           <h1>Sudoku</h1>
-
-//           <div id="gm-sudoku-board" className="gm-sudoku-board">
-//             {newSudokuBoard.map((number, index) => {
-//               return renderCell(number, index);
-//             })}
-//           </div>
-//           <button type="button" onClick={() => checkSudoku()}>
-//             <p>Comprobar</p>
-//           </button>
-//         </article>
-//         <button type="button" onClick={() => newGame()}>
-//           <p>Nueva partida</p>
-//         </button>
-//         <Link to="/">Volver</Link>
-//         {/* Lanzamos el mensaje cuando haya ganador */}
-//         {winnerMessage && (
-//           <div className="gm-hangman-winner">
-//             <p>{winnerMessage}</p>
-//             {!result && (
-//               <button type="button" onClick={resolveSudoku}>
-//                 <p>Ver la solución</p>
-//               </button>
-//             )}
-//             <button type="button" onClick={closeWinnerMessage}>
-//               <p>{result ? 'Cerrar' : 'Seguir intentándolo'}</p>
-//             </button>
-//           </div>
-//         )}
-//       </div>
-//     )
-//   );
-// }
-
-// export default Sudoku;
-
 import React, { useEffect, useState } from 'react';
 import './Sudoku.css';
 import { Link } from 'react-router-dom';
@@ -134,56 +5,58 @@ import { makepuzzle, solvepuzzle } from 'sudoku';
 
 function Sudoku() {
   const [sudokuBoard, setSudokuBoard] = useState('');
-  const [originalSudokuBoard, setOriginalSudokuBoard] = useState('');
+  const [newSudokuBoard, setNewSudokuBoard] = useState(null);
   const [winnerMessage, setWinnerMessage] = useState('');
   const [result, setResult] = useState('');
 
   useEffect(() => {
-    setSudokuBoard(makepuzzle());
-    setOriginalSudokuBoard(sudokuBoard)
+    const newSudoku = makepuzzle();
+    setSudokuBoard(newSudoku);
+    setNewSudokuBoard(newSudoku);
   }, []);
 
-  const handleInputChange = (event, index) => {
-    const newBoard = [...sudokuBoard];
-    newBoard[index] = Number(event.target.value);
-    setSudokuBoard(newBoard);
-  };
+  function newGame() {
+    const newSudoku = makepuzzle();
+    setSudokuBoard(newSudoku);
+    setNewSudokuBoard(newSudoku);
+  }
 
-  const renderCell = (number, index) => {
+  function handleInputChange(event, index) {
+    const newSudoku = [...newSudokuBoard];
+    const inputValue = event.target.value === '' ? null : Number(event.target.value);
+
+    // Validamos que el número ingresado esté entre 0 y 9
+    if (inputValue !== null && (inputValue < 0 || inputValue > 9)) {
+      alert('El número tiene que estar entre 0 y 9');
+      return;
+    }
+
+    newSudoku[index] = inputValue;
+    setNewSudokuBoard(newSudoku);
+  }
+
+  function renderCell(number, index) {
     const originalNumber = number === sudokuBoard[index] && number !== null;
+    const value = number === null ? '' : number;
 
-    if (originalNumber) {
-      return (
-        <div className="sudoku-cell-original" key={index}>
-          {number}
-        </div>
-      );
-    }
-    if (number) {
-      return (
-        <div className="sudoku-cell" key={index}>
-          {number}
-        </div>
-      );
-    }
     return (
       <input
         key={index}
+        id={index}
         type="number"
         className="sudoku-cell"
-        min="1"
+        value={value}
+        disabled={originalNumber}
+        min="0"
         max="9"
         onChange={(event) => handleInputChange(event, index)}
       />
     );
-  };
+  }
 
   function checkSudoku() {
     const solved = solvepuzzle(sudokuBoard);
-    console.log('Solved', solved);
-    console.log('Original', originalSudokuBoard);
-    console.log('Actual', sudokuBoard);
-    if (JSON.stringify(solved) === JSON.stringify(sudokuBoard)) {
+    if (solved.every((value, index) => value === newSudokuBoard[index])) {
       setWinnerMessage('Resuelto, enhorabuena');
       setResult(true);
     } else {
@@ -192,18 +65,12 @@ function Sudoku() {
     }
   }
 
-
-
-  function newGame() {
-    setSudokuBoard(makepuzzle());
-  }
-
   function closeWinnerMessage() {
     setWinnerMessage('');
   }
 
   function resolveSudoku() {
-    setSudokuBoard(solvepuzzle(sudokuBoard));
+    setNewSudokuBoard(solvepuzzle(sudokuBoard));
     closeWinnerMessage();
   }
 
@@ -212,9 +79,8 @@ function Sudoku() {
       <div className="gm-sudoku">
         <article className="gm-sudoku-game">
           <h1>Sudoku</h1>
-
           <div id="gm-sudoku-board" className="gm-sudoku-board">
-            {sudokuBoard.map((number, index) => {
+            {newSudokuBoard.map((number, index) => {
               return renderCell(number, index);
             })}
           </div>
